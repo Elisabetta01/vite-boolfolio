a6320012ec3bc778a4a8ba1857de8307d44a811d
Elisabetta01 <scagione.elisabetta@gmail.com>
2023-06-30 17:27:57 +0200

    milestone 4: chiamata api e console.log

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <script >
 
+import axios from 'axios';
+
+export default{
+  data(){
+    return{
+      posts: []
+    }
+  },
+
+  mounted(){
+    this.getPosts()
+  },
+
+  methods: {
+    getPosts(){
+      axios.get('http://127.0.0.1:8000/api/posts')
+        .then( res => {
+          this.posts = res.data.posts
+          console.log(res.data)
+        })
+    }
+  },
+}
+
 </script>
 
 <template>
